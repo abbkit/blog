38110625a2d1eee0c280d86cf1525312f6834fab
3cgg@163.com <3cgg@163.com>
2022-11-20 11:13:15 +0800

    修改sh脚本

--- server/rebuild-start.sh
+++ server/rebuild-start.sh
@@ -2,8 +2,8 @@
 
 #https://ghproxy.com/
 #git clone https://ghproxy.com/https://github.com/abbkit/blog.git
-mvn clean install -Dmaven.test.skip=true
-docker build --network=abbkit -t abbkit:0.0.1 .
 docker stop abbkit
 docker rm abbkit
+mvn clean install -Dmaven.test.skip=true
+docker build --network=abbkit -t abbkit:0.0.1 .
 docker-compose -f abbkit-compose.yml up -d
