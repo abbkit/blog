5faa4920957656a170cb3fe663ca01a4c4dd604e
3cgg@163.com <3cgg@163.com>
2022-11-25 17:14:56 +0800

    no message

---
+++ server/server-starter/src/test/java/test/com/abbkit/project/TestApplication.java
@@ -0,0 +1,12 @@
+package test.com.abbkit.project;
+
+import com.abbkit.project.Application;
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
